b1b3806d6811496dc6b4c068e74e488953f4c386
Henry Taylor <info@audiostems.co.uk>
2025-08-08 13:13:09 +0100

    FINAL VERCEL FIX: React exact versions + resolutions

--- audiostems-frontend/components/auth/LoginButton.js
+++ audiostems-frontend/components/auth/LoginButton.js
@@ -1,4 +1,4 @@
-import { useAuth0 } from '@auth0/auth0-react';
+import { useAuth0 } from '../../lib/auth0-stub';
 import { Button } from 'flowbite-react';
 
 export default function LoginButton({ className = '', children = 'Log In' }) {
